7e347b7301c9fff9c38e00f1b8f5ce8340c490e9
Bruno Marques <brunoliveira.marques@gmail.com>
2021-05-04 11:16:07 -0300

    DAO atualizado com crud completo

--- br.com.proway.senior.fechamentoFolha/src/main/java/br/com/proway/senior/model/InterfaceFolhaDAO.java
+++ br.com.proway.senior.fechamentoFolha/src/main/java/br/com/proway/senior/model/InterfaceFolhaDAO.java
@@ -1,23 +1,13 @@
 package br.com.proway.senior.model;
 
-import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public interface InterfaceFolhaDAO {
 	
 	public ArrayList<ArrayList<String>> getAll();
-	
 	public Folha getFolhasPorId(int id);
-	
-	public Folha getFolhaPorDataEId(String data, int id);
-
 	public void saveFolha(Folha folha);
-	
-	public boolean updateFolha(Folha folha, int id);
-	
-	public boolean removeFolha(int id);
-	
-	public ArrayList<Folha> getFolhaIdColaborador(Integer idColaborador);
-	
-	
+	public void updateFolha(Folha folha, int id);
+	public void deleteFolha(int id);
+	public ArrayList<Folha> getFolhasPorColaborador(int idColaborador);
 }
