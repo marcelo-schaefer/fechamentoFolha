5b4f0e30733a0c69f7bd65fde115c975331c94fa
Lucas Grijó <rksgrijo@gmail.com>
2021-05-04 14:24:14 -0300

    JavaDocs de tudo exceto os calculos, DAO e testes.

--- br.com.proway.senior.fechamentoFolha/src/main/java/br/com/proway/senior/model/FolhaBuilder.java
+++ br.com.proway.senior.fechamentoFolha/src/main/java/br/com/proway/senior/model/FolhaBuilder.java
@@ -11,63 +11,102 @@ import br.com.proway.senior.model.externo.IColaboradorFolha;
 import br.com.proway.senior.model.externo.IFeriasFolha;
 import br.com.proway.senior.model.externo.IPontoFolha;
 
+/**
+ * FolhaBuilder
+ * 
+ * É uma classe que contém a lógica de construção de uma Folha seguindo o design
+ * pattern builder.
+ * 
+ * @author Lucas Grijó
+ * @author Lucas Walim
+ * @author Marcelo Schaefer
+ */
 public class FolhaBuilder implements IFolhaBuilder {
 
 	private int id; //
-	private Integer idColaborador; 
-	private LocalDate dataEmissao; 
-	
+	private Integer idColaborador;
+	private LocalDate dataEmissao;
 	// Folha Normal
 	private double valorHorasTrabalhadas;
-	private double valorHorasFaltas; 
-	private double valorHorasExtras; 
+	private double valorHorasFaltas;
+	private double valorHorasExtras;
 	private double valorReflexoDSR;
 	private double valorInss;
-	private double valorImpostoDeRenda;	
-	private double valorPlanoSaude; 
+	private double valorImpostoDeRenda;
+	private double valorPlanoSaude;
 	private double valorValeTransporte;
 	private double salarioBruto = 0;
 	private double salarioLiquido;
-	
 	// Folha Férias
-	private double valorFerias; // férias bruto.
+	private double valorFerias;
 	private double valorInssFerias;
 	private double valorImpostoDeRendaFerias;
 	private double feriasLiquido;
-		
-	// Dados de Criação
+	// Dados para construção/calculo.
 	private ICalculoHoras calculoHoras;
 	private ICalculoDesconto calculoDesconto;
 	private double valorHora;
 
+	/**
+	 * build
+	 * 
+	 * Constrói a folha com os dados calculados. Também determina a data de emissão.
+	 * 
+	 * @author Lucas Grijó
+	 * @author Lucas Walim
+	 * @author Marcelo Schaefer
+	 */
 	public Folha build() {
 		dataEmissao = LocalDate.now();
-		return new Folha(id, idColaborador, dataEmissao, valorHorasTrabalhadas,
-				valorHorasFaltas, valorHorasExtras, valorReflexoDSR, valorInss,
-				valorImpostoDeRenda, valorPlanoSaude, valorValeTransporte, salarioBruto,
-				salarioLiquido, valorFerias, valorInssFerias, valorImpostoDeRendaFerias,
-				feriasLiquido);
+		return new Folha(id, idColaborador, dataEmissao, valorHorasTrabalhadas, valorHorasFaltas, valorHorasExtras,
+				valorReflexoDSR, valorInss, valorImpostoDeRenda, valorPlanoSaude, valorValeTransporte, salarioBruto,
+				salarioLiquido, valorFerias, valorInssFerias, valorImpostoDeRendaFerias, feriasLiquido);
 	}
-	
+
+	/**
+	 * Inicializa Calculos
+	 * 
+	 * Realiza os calculos inicias e instancia os objetos necessários para o calculos de qualquer tipo de folha.
+	 * 
+	 * @author Lucas Grijó
+	 * @author Lucas Walim
+	 * @author Marcelo Schaefer
+	 */
 	public void iniciarCalculos(IColaboradorFolha colaborador, ICargoFolha cargo) {
 		idColaborador = colaborador.getId();
 		this.calculoHoras = new CalculoHoras();
 		this.calculoDesconto = new CalculoDesconto();
 		valorHora = (calculoHoras.calcularValorHora(cargo));
-		
+
 	}
 
-	// FOLHA NORMAL
-	
-	public void calcularHorasNormais(IPontoFolha ponto, ICargoFolha cargo){	
+	/**
+	 * Calculo das Horas Normais
+	 * 
+	 * Realiza os calculo do valor recebido pertinente a horas trabalhadas, faltas, horas extras e reflexo DSR.
+	 * 
+	 * @author Lucas Grijó
+	 * @author Lucas Walim
+	 * @author Marcelo Schaefer
+	 */
+	public void calcularHorasNormais(IPontoFolha ponto, ICargoFolha cargo) {
 		valorHorasTrabalhadas = (calculoHoras.calcularValorDasHorasTrabalhadas(ponto, valorHora));
 		valorHorasFaltas = (calculoHoras.calcularValorHorasFaltas(ponto, valorHora));
 		valorHorasExtras = (calculoHoras.calcularValorHorasExtras(ponto, valorHorasTrabalhadas, valorHora));
 		valorReflexoDSR = (calculoHoras.calcularDSR(valorHorasExtras));
 		salarioBruto = (valorHorasTrabalhadas - valorHorasFaltas + valorHorasExtras + valorReflexoDSR);
 	}
-	
-	public void calcularDescontoNormal(IColaboradorFolha colaborador, ICargoFolha cargo){
+
+	/**
+	 * Calculo de descontos em folha normal.
+	 * 
+	 * Atribui descontos no salario pertinente a inss, imposto de renda, plano de saude e vale transporte. Também determina o salário liquido.
+	 * 
+	 * @author Lucas Grijó
+	 * @author Lucas Walim
+	 * @author Marcelo Schaefer
+	 */
+	public void calcularDescontoNormal(IColaboradorFolha colaborador, ICargoFolha cargo) {
 		valorInss = (calculoDesconto.calcularDescontoInss(salarioBruto));
 		salarioLiquido = (salarioBruto - valorInss);
 		valorImpostoDeRenda = (calculoDesconto.calcularDescontoImpostoRenda(colaborador, salarioLiquido));
@@ -75,19 +114,34 @@ public class FolhaBuilder implements IFolhaBuilder {
 		valorValeTransporte = (calculoDesconto.calcularDescontoValeTransporte(colaborador, cargo));
 		salarioLiquido = (salarioBruto - valorValeTransporte - valorImpostoDeRenda - valorPlanoSaude);
 	}
-	
-	// FOLHA FERIAS
-	
+
+	/**
+	 * Calculo das horas férias
+	 * 
+	 * Realiza os calculo do valor recebido pertinente a horas de férias.
+	 * 
+	 * @author Lucas Grijó
+	 * @author Lucas Walim
+	 * @author Marcelo Schaefer
+	 */
 	public void calcularHorasFerias(IFeriasFolha feriasFolha) {
 		valorFerias = (calculoHoras.calcularFerias(feriasFolha.getDias(), feriasFolha.getAbono(), valorHora));
 	}
-	
+
+	/**
+	 * Calculo de descontos em folha férias.
+	 * 
+	 * Atribui descontos no valor das férias pertinente a inss e imposto de renda. Também determina a férias liquido.
+	 * 
+	 * @author Lucas Grijó
+	 * @author Lucas Walim
+	 * @author Marcelo Schaefer
+	 */
 	public void calcularDescontoFerias(IColaboradorFolha colaborador) {
 		valorInssFerias = (calculoDesconto.calcularDescontoInss(valorFerias));
 		feriasLiquido = (valorFerias - valorInssFerias);
 		valorImpostoDeRendaFerias = (calculoDesconto.calcularDescontoImpostoRenda(colaborador, feriasLiquido));
 		feriasLiquido = (feriasLiquido - valorImpostoDeRendaFerias);
 	}
-	
 
 }
--- br.com.proway.senior.fechamentoFolha/src/main/java/br/com/proway/senior/model/externo/CargoFolha.java
+++ br.com.proway.senior.fechamentoFolha/src/main/java/br/com/proway/senior/model/externo/CargoFolha.java
@@ -1,21 +1,23 @@
 package br.com.proway.senior.model.externo;
 
+/**
+ * CargoFolha
+ * 
+ * Objeto conténdo os dados necessários de um cargo para utilização interna na
+ * criação de uma folha.
+ * 
+ * @author sprint3
+ */
 public class CargoFolha implements ICargoFolha {
 
 	private double salarioBase;
 	private double percentualInsalubridade;
 
-	/**
-	 * 
-	 * @param salarioBase
-	 * @param valorBonificacao
-	 * @param percentualInsalubridade
-	 */
 	public CargoFolha(double salarioBase, double percentualInsalubridade) {
 		this.salarioBase = salarioBase;
 		this.percentualInsalubridade = percentualInsalubridade;
 	}
-	
+
 	public double getSalarioBase() {
 		return salarioBase;
 	}
