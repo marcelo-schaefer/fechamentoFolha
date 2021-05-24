a311f151fce67c5b9d522bbfdb19706ca24eaa25
Sabrina <sabrinaschmidt335@gmail.com>
2021-05-24 17:49:12 -0300

    Alterações na FolhaControllerApi

--- br.com.proway.senior.fechamentoFolha/src/main/java/br/com/proway/senior/controller/FolhaControllerAPI.java
+++ br.com.proway.senior.fechamentoFolha/src/main/java/br/com/proway/senior/controller/FolhaControllerAPI.java
@@ -4,7 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.proway.senior.dto.FolhaDto;
+import br.com.proway.senior.model.Bonificacao;
 import br.com.proway.senior.model.Folha;
+import br.com.proway.senior.model.FolhaBuilder;
+import br.com.proway.senior.model.FolhaDirector;
+import br.com.proway.senior.model.externo.interfaces.ICargoFolha;
+import br.com.proway.senior.model.externo.interfaces.IColaboradorFolha;
+import br.com.proway.senior.model.externo.interfaces.IFeriasFolha;
+import br.com.proway.senior.model.externo.interfaces.IPontoFolha;
 
 public class FolhaControllerAPI {
 
@@ -22,6 +29,27 @@ public class FolhaControllerAPI {
 	public FolhaDto buscarFolhasPorId(Integer id) {
 		FolhaDto folha = new FolhaDto(folhaController.getById(id));
 		return folha;
+	}
+	
+	public FolhaDto construirFolhaNormal(IColaboradorFolha colaborador, 
+			IPontoFolha ponto, ICargoFolha cargo, Bonificacao bonificacao) {
+		FolhaDto folhaNormal = new FolhaDto(folhaController.
+				construirFolhaNormal(colaborador, ponto, cargo, bonificacao));
+		return folhaNormal;
+	}
+	
+	public FolhaDto construirFolhaFerias(IColaboradorFolha colaborador,
+			IFeriasFolha ferias, ICargoFolha cargo) {
+		FolhaDto folhaFerias = new FolhaDto(folhaController.
+				construirFolhaFerias(colaborador,  ferias,  cargo));
+		return folhaFerias;
+	}
+	
+	public FolhaDto construirFolhaHibrida(IColaboradorFolha colaborador, 
+			IPontoFolha ponto, ICargoFolha cargo, IFeriasFolha ferias) {
+		FolhaDto folhaHibrida = new FolhaDto(folhaController.
+				construirFolhaHibrida( colaborador,  ponto,  cargo, ferias));
+		return folhaHibrida;
 	}
 
 }
