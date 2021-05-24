4262c096f137e8d712a5143a70b9d2eff71ecf38
SENIOR <SENIOR@proway.treina>
2021-05-24 13:18:16 -0300

    Criacao dos metodos FolhaControllerApi

--- br.com.proway.senior.fechamentoFolha/src/main/java/br/com/proway/senior/controller/FolhaControllerAPI.java
+++ br.com.proway.senior.fechamentoFolha/src/main/java/br/com/proway/senior/controller/FolhaControllerAPI.java
@@ -1,34 +1,31 @@
 package br.com.proway.senior.controller;
 
-import br.com.proway.senior.dao.FolhaDAO;
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.proway.senior.dto.FolhaDto;
-import br.com.proway.senior.model.Bonificacao;
 import br.com.proway.senior.model.Folha;
-import br.com.proway.senior.model.FolhaBuilder;
-import br.com.proway.senior.model.FolhaDirector;
-import br.com.proway.senior.model.externo.interfaces.ICargoFolha;
-import br.com.proway.senior.model.externo.interfaces.IColaboradorFolha;
-import br.com.proway.senior.model.externo.interfaces.IPontoFolha;
 
 public class FolhaControllerAPI {
 	
 	FolhaController folhaController = new FolhaController();
 	
-	public Folha construirFolhaNormal(IColaboradorFolha colaborador, IPontoFolha ponto, ICargoFolha cargo, Bonificacao bonificacao) {
-		FolhaBuilder folhaBuilder = new FolhaBuilder();
-		FolhaDirector director = new FolhaDirector(folhaBuilder);
-		return folhaBuilder.build();
+	public  List<FolhaDto> buscarTodasFolhas() {
+		List<FolhaDto> listaFolhaDTO = new ArrayList<FolhaDto>();
+		List<Folha> lista = folhaController.getAll();
+		for (Folha f : lista) {
+			listaFolhaDTO.add(new FolhaDto(f));
+		}
+		return listaFolhaDTO;
 	}
-	public FolhaDto salvarFolha(FolhaDto folhaDto) {
-		return new FolhaDto(folhaController.salvarFolha(folhaDto));
+	
+	public FolhaDto buscarFolhasPorId(Integer idEmpresa) {
+		FolhaDto folha = new FolhaDto(folhaController.getById(idEmpresa));
+		return folha;
 	}
-
-	public void editarFolha(Folha folha) {
-		
-	}
-
-	public void deletarFolha(Folha folha) {
-	}
+	
+	
+	
 }
 
 
